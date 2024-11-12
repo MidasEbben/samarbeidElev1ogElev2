69cf4844f3171aabc4c0252c47a588f00a372e20
MidasEbben <mid1505@skole.telemarkfylke.no>
2024-11-12 08:32:07 +0100

    bil.js
    dkdkdkdkd

---
+++ bil/bil.js
@@ -0,0 +1,9 @@
+//deklarere variabler alder
+//dersom alder < 18
+    //skriv tilbakemelding
+//dersom alder > 17 
+    //skriv tilbakemelding 
+//dersom alder > 6
+    //skriv tilbakemelding
+//dersom alder < 70
+    //skriv tilbakemelding
